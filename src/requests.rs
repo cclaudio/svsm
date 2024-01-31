890ea746fa2ec296311cca6fd7672b60b03dba35
Claudio Carvalho <cclaudio@linux.ibm.com>
2024-01-31 05:48:20 +0200

    protocols: Implement the vTPM protocol
    The vTPM protocol is introduced in the SVSM specification v1.0 to allow other
    software components (e.g. OVMF and guest kernel) to communicate with the
    SVSM vTPM at runtime. It follows the Microsoft TPM 2.0 Simulator protocol.
    
    The specification enumerates two call IDs supportted by the vTPM protocol:
    1. SVSM_VTPM_QUERY: query vTPM command and features support.
    2. SVSM_VTPM_CMD: Execute a TPM platform command.
    
    As for the SVSM_VTPM_CMD, the TPM platform commands are described in
    libvtpm/ms-tpm-20-ref/TPMCmd/Simulator/include/prototypes/Simulator_fp.h
    e.g.:
    - TPM_SIGNAL_HASH_DATA
    - TPM_SEND_COMMAND
    - TPM_REMOTE_HANDSHAKE
    - TPM_SET_ALTERNATIVE_RESULT
    
    This patch implements both the SVSM_VTPM_QUERY and the SVSM_VTPM_CMD call IDs.
    
    For the SVSM_VTPM_CMD, only the TPM_SEND_COMMAND platform command is
    implemented.
    
    Signed-off-by: Claudio Carvalho <cclaudio@linux.ibm.com>

--- src/requests.rs
+++ src/requests.rs
@@ -11,6 +11,8 @@ use crate::mm::GuestPtr;
 use crate::protocols::core::core_protocol_request;
 use crate::protocols::errors::{SvsmReqError, SvsmResultCode};
 
+#[cfg(feature = "vtpm")]
+use crate::protocols::{vtpm::vtpm_protocol_request, SVSM_VTPM_PROTOCOL};
 use crate::protocols::{RequestParams, SVSM_CORE_PROTOCOL};
 use crate::types::GUEST_VMPL;
 use crate::utils::halt;
@@ -72,6 +74,8 @@ fn request_loop_once(
 
     match protocol {
         SVSM_CORE_PROTOCOL => core_protocol_request(request, params).map(|_| true),
+        #[cfg(feature = "vtpm")]
+        SVSM_VTPM_PROTOCOL => vtpm_protocol_request(request, params).map(|_| true),
         _ => Err(SvsmReqError::unsupported_protocol()),
     }
 }
